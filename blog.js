fa21d80497f3d5291f49e5fe83b419cae682541c
apalm112 <apalm112@gmail.com>
2016-01-05 19:56:56 -0800

    Starting to add code for the Home & About tabs to the JS, CSS & HTML files.

--- blog.js
+++ blog.js
@@ -16,5 +16,22 @@ Portfolio.prototype.toHtml = function() {
   $newPortfolio.data('title', this.title);
   $newPortfolio.data('body', this.body);
   $newPortfolio.data('time', this.time);
-  
+
 };
+
+/*DAY 2
+As the creator, I want the Home and About nav links to act as tabs, so my story is revealed FAST.
+Add tabs for navigation */
+$('.tabs .tab-links a').on('click', function(e) {
+    var currentAttrValue = $(this).attr('href');
+
+    // Show/hide tabs
+    $('.tabs ' + currentAttrValue).show().siblings().hide();
+
+    //change/remove current tab to active
+    $(this).parent('li').addClass('active').siblings().removeClass('active');
+
+});
+
+
+/* As a reader, I want the portfolio to use a nice color scheme, so that it stands out visually.*/

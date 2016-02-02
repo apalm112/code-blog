dc2605cdb79fe6069b0da71e888f43518ce62ae5
apalm112 <apalm112@gmail.com>
2016-02-01 23:01:07 -0800

    added github token

--- scripts/repo.js
+++ scripts/repo.js
@@ -4,19 +4,26 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    $.ajax({
-      url: '/github/users/apalm112/repos' + '?per_page=100' + '&sort=updated',
-      type: 'GET',
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
-    }).done(callback);
-  };
+    var qs = '?per_page=100&sort=updated';
 
-  repos.with = function(attr) {
-    return repos.all.filter(function(repo) {
-      return repo[attr];
+    $.ajax({
+      url: 'https://api.github.com/users/apalm112/repos' + '?per_page=5&sort=updated',
+      type: 'GET',
+      headers: {'Authorization': 'token ' + GITHUB_TOKEN},
+      success: function(data, message, xhr) {
+        console.log('data');
+        repos.all = data;
+        callback();
+      }
     });
+
+
+    repos.with = function(attr) {
+      return repos.all.filter(function(repo) {
+        return repo[attr];
+      });
+
+    };
   };
 
   module.repos = repos;

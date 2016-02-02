7b3de2dc6ee7b18750f035744a1dbef3ee791fd3
apalm112 <apalm112@gmail.com>
2016-02-02 10:45:22 -0800

    changed API call in repo.js to meet MVP goal.

--- scripts/repo.js
+++ scripts/repo.js
@@ -4,24 +4,28 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    var qs = '?per_page=100&sort=updated';
+  //  var qs = '?per_page=100&sort=updated';  //taken out for class-13
 
     $.ajax({
-      url: 'https://api.github.com/users/apalm112/repos' + '?per_page=5&sort=updated',
+      url: 'https://api.github.com/users/apalm112/repos' +
+           '?per_page=5' +
+           '&sort=updated',
       type: 'GET',
-      headers: {'Authorization': 'token ' + GITHUB_TOKEN},
+    //  headers: {'Authorization': 'token ' + GITHUB_TOKEN},
       success: function(data, message, xhr) {
         repos.all = data;
-        callback();
-        console.log('data');
+       /* callback();
+        console.log('data');*/
       }
-    });
+    }).done(callback);
+  };
+
     repos.with = function(attr) {
       return repos.all.filter(function(repo) {
         return repo[attr];
       });
     };
-  };
+
 
   module.repos = repos;
 })(window);
